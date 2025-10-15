ed6b1dd90c7877d1e83c4585ce037933d80db6b6
Hash-Brown15 <JackHeppell1@gmail.com>
2025-10-15 20:32:11 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -1,6 +1,17 @@
 from tkinter import *
 import tkinter.font as font 
 
+def convert():
+    temp_celsius = celsius_value.get()
+
+    if(temp_celsius.replace('.','',1).isdigit()):
+        error_msg.grid_forget()
+        temp_farenheit = (float(temp_celsius) * 9/5) + 32
+        output_farenheit.config(text = f"Temperature in Farenheit : "+str(temp_farenheit))
+
+    else:
+        error_msg.grid(row=1, column=1)
+
 my_window = Tk()
 my_window.geometry("300x300")
 
@@ -21,8 +32,8 @@ error_msg = Label(frame,text="Please enter valid input", font=font.Font(size=8),
 output_farenheit = Label(frame, font=font.Font(size=12))
 output_farenheit.grid(row=2, column = 0, columnspan=2, pady=10)
 
-submit_btn = Button(frame, text="Convert", width = 30, fg = "Black", bg = "light green", bd = 0, padx = 20, pady = 10= command = convert)
+submit_btn = Button(frame, text="Convert", width = 30, fg = "Black", bg = "light green", bd = 0, padx = 20, pady = 10, command = convert)
 submit_btn.grid(row = 3, column = 0, columnspan = 2, pady = 10)
 
-my_window.geomitry("500x250")
+my_window.geometry("500x250")
 my_window.mainloop()
